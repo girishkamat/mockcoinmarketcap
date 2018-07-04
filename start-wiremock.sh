7520b2d88439659e70bf604e4c39910982de0886
Girish Kamath <girish.kamat@gmail.com>
2018-07-04 19:11:47 +0100

    Fixed issue with num of cryptos in metadata

--- start-wiremock.sh
+++ start-wiremock.sh
@@ -3,8 +3,8 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-# echo -e "Downloading wiremock standalone jar"
-# wget -N http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.17.0/wiremock-standalone-2.17.0.jar
+echo -e "Downloading wiremock standalone jar"
+wget -N http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.17.0/wiremock-standalone-2.17.0.jar
 
 echo "Starting wiremock server"
 java -jar wiremock-standalone-2.17.0.jar --port 9000 --verbose --local-response-templating --root-dir . &
